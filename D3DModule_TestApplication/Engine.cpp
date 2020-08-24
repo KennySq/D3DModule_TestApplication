bda2b0a16da4492d3797ae1b87109b6ba8e89ba0
PeaceKeeper <odessa5d4@gmail.com>
2020-08-25 02:42:27 +0900

    이니셜 커밋

---
+++ D3DModule_TestApplication/Engine.cpp
@@ -0,0 +1,39 @@
+#include"pch.h"
+#include "Engine.h"
+
+shared_ptr<Engine> Engine::EngineInstance = nullptr;
+
+bool Engine::OnInit()
+{
+	FBXLoader Loader;
+	Camera = make_shared<D3DACamera>(D3DACamera());
+	shared_ptr<D3DAModel<D3DVERTEX::StandardVertex>> SampleModel = Loader.Load("Resources/Stylized_box.FBX");
+	SwapChainTexture = Renderer->GetSwapChainBuffer(0);
+	Renderer->AddRenderViewport(1.0f, 1.0f, BIWidth, BIHeight, 1.0f);
+	
+	static shared_ptr<D3DRS::D3DRenderableTexture> SwapChainTexture = Renderer->GetSwapChainBuffer();
+	static ComPtr<ID3D11RenderTargetView> SwapChainRTV = SwapChainTexture->GetResource();
+	//static shared_ptr<D3DRS::D3DDepthStencilTexture> DepthStencilTexture = Renderer->GetSwapChainDepthStencil();
+
+	Context->RSSetViewports(0, &Renderer->GetViewport(0));
+	//Context->OMSetRenderTargets(1, SwapChainRTV.GetAddressOf(), DepthStencilTexture->GetResource().Get());
+	return true;
+}
+
+void Engine::OnUpdate(float Delta)
+{	
+	static ComPtr<ID3D11RenderTargetView> SwapChainRTV = SwapChainTexture->GetResource();
+	static shared_ptr<D3DRS::D3DDepthStencilTexture> DepthStencilTexture = Renderer->GetSwapChainDepthStencil();
+
+	Renderer->ClearTexture2D(SwapChainTexture, Colors::Red);
+	Renderer->ClearDepthStencil(DepthStencilTexture);
+}
+
+void Engine::OnRender(float Delta)
+{
+	Renderer->Execute();
+	SwapChain->Present(0, 0);
+}
+
+void Engine::OnRelease()
+{}
---
+++ D3DModule_TestApplication/Engine.h
@@ -0,0 +1,48 @@
+#pragma once
+class Engine : public VirtualObject
+{
+private:
+	
+	shared_ptr<D3DHWDevice> HWDevice;
+	shared_ptr<RenderManager> Renderer;
+	ComPtr<ID3D11Device> Device;
+	ComPtr<ID3D11DeviceContext> Context;
+	ComPtr<IDXGISwapChain> SwapChain;
+
+	shared_ptr<D3DRenderableTexture> SwapChainTexture;
+	
+	static shared_ptr<Engine> EngineInstance;
+	shared_ptr<D3DARS::D3DACamera> Camera;
+	
+	Engine()
+	{
+		HWDevice = D3DHWDevice::GetHWDevice();
+		Device = D3DHWDevice::GetDevice();
+		Context = D3DHWDevice::GetContext();
+		SwapChain = D3DHWDevice::GetSwapChain();
+		Renderer = make_shared<RenderManager>();
+		Camera = make_shared<D3DACamera>();
+	}
+
+public:
+
+
+private:
+	virtual bool OnInit() override;
+	virtual void OnUpdate(float Delta) override;
+	virtual void OnRender(float Delta) override;
+	virtual void OnRelease() override;
+
+public:
+
+	~Engine() {}
+
+	static shared_ptr<Engine> GetEngineInstance() {
+		if (EngineInstance == nullptr) 
+			EngineInstance = make_shared<Engine>(Engine());
+	
+		return EngineInstance;
+	}
+
+};
+
