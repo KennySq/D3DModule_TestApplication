f0ffc2b79990924af07f9190d17ceea9372b1d80
PeaceKeeper <odessa5d4@gmail.com>
2020-08-26 02:02:04 +0900

    스왑체인 텍스쳐 생성 파라미터 재구성

--- D3DModule_TestApplication/Engine.cpp
+++ D3DModule_TestApplication/Engine.cpp
@@ -8,23 +8,27 @@ bool Engine::OnInit()
 	FBXLoader Loader;
 	Camera = make_shared<D3DACamera>(D3DACamera());
 	shared_ptr<D3DAModel<D3DVERTEX::StandardVertex>> SampleModel = Loader.Load("Resources/Stylized_box.FBX");
-	SwapChainTexture = Renderer->GetSwapChainBuffer(0);
+	Renderer->GetSwapChainBuffer(0, SwapChainTexture);
 	Renderer->AddRenderViewport(1.0f, 1.0f, BIWidth, BIHeight, 1.0f);
 	
 	static shared_ptr<D3DRS::D3DRenderableTexture> SwapChainTexture;
 	Renderer->GetSwapChainBuffer(0, SwapChainTexture);
-	static ComPtr<ID3D11RenderTargetView> SwapChainRTV = SwapChainTexture->GetResource();
+	static ComPtr<ID3D11RenderTargetView> SwapChainRTV = *SwapChainTexture->GetResource();
 	static shared_ptr<D3DRS::D3DDepthStencilTexture> DepthStencilTexture;
-	Renderer->GetSwapChainDepthBuffer();
+	Renderer->GetSwapChainDepthBuffer(DepthStencilTexture);
+
+	Renderer->SetSwapChainTexture(SwapChainTexture);
+	Renderer->SetSwapChainDepthStencil(DepthStencilTexture);
+
 
 	Context->RSSetViewports(0, &Renderer->GetViewport(0));
-	Context->OMSetRenderTargets(1, SwapChainRTV.GetAddressOf(), DepthStencilTexture->GetResource().Get());
+	Context->OMSetRenderTargets(1, SwapChainRTV.GetAddressOf(), *DepthStencilTexture->GetResource());
 	return true;
 }
 
 void Engine::OnUpdate(float Delta)
 {	
-	static ComPtr<ID3D11RenderTargetView> SwapChainRTV = SwapChainTexture->GetResource();
+	static ComPtr<ID3D11RenderTargetView> SwapChainRTV = *SwapChainTexture->GetResource();
 	static shared_ptr<D3DRS::D3DDepthStencilTexture> DepthStencilTexture = Renderer->GetSwapChainDepthStencil();
 
 	Renderer->ClearTexture2D(SwapChainTexture, Colors::Red);
